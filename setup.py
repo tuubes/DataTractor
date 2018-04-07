bc901e4b4fc36fb3d3687f3c71afebfc532e4bde
Guillaume R <theelectronwill@gmail.com>
2018-04-07 15:46:02 +0200

    Setup python project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='DataTractor',
+	version='0.1',
+	packages=['datatractor.mcpc'],
+	url='https://github.com/tuubes/DataTractor',
+	license='GPLv3',
+	author='TheElectronWill',
+	description='Data extractor for Tuubes',
+	install_requires=['requests-html']
+)
