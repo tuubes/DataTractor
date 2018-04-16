44e8152caec5bfa6d8b596544adb24cd73e77b0e
Guillaume R <theelectronwill@gmail.com>
2018-04-16 22:53:55 +0200

    Test scala_type(str)

--- datatractor/test/test_extractor.py
+++ datatractor/test/test_extractor.py
@@ -1,5 +1,10 @@
-from datatractor.main import packets_extractor
+from datatractor.main import packets_extractor, scala_writer
 
 game_version = input("Game Version: ").strip()
 protocol = packets_extractor.extract_packets(game_version)
 print(repr(protocol))
+
+for packet in protocol.play.clientbound:
+	print("====", packet.name, "====")
+	for field in packet.fields:
+		print(field.name, ":", scala_writer.scala_type(field.type))
