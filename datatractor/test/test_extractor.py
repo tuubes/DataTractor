43b22dc7ce7ac49d92b462ffd6f934b0ec1ee0e1
Guillaume R <theelectronwill@gmail.com>
2018-04-16 23:52:43 +0200

    Rename scala_writer to scala_generator

--- datatractor/test/test_extractor.py
+++ datatractor/test/test_extractor.py
@@ -1,4 +1,4 @@
-from datatractor.main import packets_extractor, scala_writer
+from datatractor.main import packets_extractor, scala_generator
 
 game_version = input("Game Version: ").strip()
 protocol = packets_extractor.extract_packets(game_version)
@@ -8,7 +8,7 @@ for packet in protocol.play.clientbound:
 	print("====", packet.name, "====")
 	for field in packet.fields:
 		comment = "" if (field.comment is None) else field.comment
-		stype = scala_writer.scala_type(field.type)
+		stype = scala_generator.scala_type(field.type)
 		a = "%s : %s" % (field.name, stype)
 		aligned = a + " " * (max(0, 30 - len(a)))
 		print("%s || %s" % (aligned, comment))
