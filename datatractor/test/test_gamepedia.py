44c73e34c487c49292fe23b4466907e2a198f905
Guillaume R <theelectronwill@gmail.com>
2018-04-17 14:59:44 +0200

    Test wiki extractor

---
+++ datatractor/test/test_gamepedia.py
@@ -0,0 +1,19 @@
+import requests_cache
+
+from datatractor.utils.gamepedia_wiki_tools import *
+
+requests_cache.install_cache("out/http_cache", "sqlite", 300)
+
+version = "1.11"
+release_date, next_version, next_date = extract_release_infos(version, True)
+print("Version: %s released on %s" % (version, release_date))
+print("Next version: %s released on %s" % (next_version, next_date))
+
+blocks_url = get_revision_url("Java_Edition_data_values/Block_IDs", release_date, next_date)
+print("Block IDs for version %s : %s" % (version, blocks_url))
+
+items_url = get_revision_url("Java_Edition_data_values/Item_IDs", release_date, next_date)
+print("Item IDs for version %s : %s" % (version, items_url))
+
+entities_url = get_revision_url("Java_Edition_data_values/Entity_IDs", release_date, next_date)
+print("Entity IDs for version %s : %s" % (version, entities_url))
