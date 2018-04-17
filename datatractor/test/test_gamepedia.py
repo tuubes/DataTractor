38d7fdc8058828fb686f912ea047bc00b2bbd401
Guillaume R <theelectronwill@gmail.com>
2018-04-17 16:34:09 +0200

    Test a block wiki page

--- datatractor/test/test_gamepedia.py
+++ datatractor/test/test_gamepedia.py
@@ -10,10 +10,13 @@ print("Version: %s released on %s" % (version, release_date))
 print("Next version: %s released on %s" % (next_version, next_date))
 
 blocks_url = get_revision_url("Java_Edition_data_values/Block_IDs", next_date)
-print("Block IDs for version %s : %s" % (version, blocks_url))
+print("Block IDs for version %s: %s" % (version, blocks_url))
 
 items_url = get_revision_url("Java_Edition_data_values/Item_IDs", next_date)
-print("Item IDs for version %s : %s" % (version, items_url))
+print("Item IDs for version %s: %s" % (version, items_url))
 
 entities_url = get_revision_url("Java_Edition_data_values/Entity_IDs", next_date)
-print("Entity IDs for version %s : %s" % (version, entities_url))
+print("Entity IDs for version %s: %s" % (version, entities_url))
+
+stone_url = get_revision_url("Furnace", next_date)
+print("Furnace for version %s: %s" % (version, stone_url))
