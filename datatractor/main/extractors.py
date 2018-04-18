d380052341d2b07038a43f50f656e42e10b630e6
Guillaume R <theelectronwill@gmail.com>
2018-04-19 01:04:15 +0200

    Fix file writing

--- datatractor/main/extractors.py
+++ datatractor/main/extractors.py
@@ -55,8 +55,8 @@ class BlocksExtractor:
 
 	def extract(self, output_dir):
 		blocks = b_extractor.extract_blocks(self.next_date)
-		f = open("%s/blocks.json" % output_dir)
-		f2 = open("%s/blocks_legacy.json" % output_dir)
+		f = open("%s/blocks.json" % output_dir, "w")
+		f2 = open("%s/blocks_legacy.json" % output_dir, "w")
 		for block in blocks:
 			json = b_extractor.json_block_variants(block)
 			f.write(json)
