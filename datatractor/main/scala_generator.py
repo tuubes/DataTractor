2d3837556ec10545f81665e12e505b60a6d1c8c1
Guillaume R <theelectronwill@gmail.com>
2018-05-11 20:02:35 +0200

    Use 2 spaces to indent scala code

--- datatractor/main/scala_generator.py
+++ datatractor/main/scala_generator.py
@@ -89,14 +89,14 @@ def niol_write(field_type: str) -> str:
 	if t.startswith("optional "):
 		x = t[9:]
 		write_x = niol_write(x)
-		return "if ($.isDefined) {\n\t\t\t%s\n\t\t}" % write_x
+		return "if ($.isDefined) {\n      %s\n    }" % write_x
 
 	if t.startswith("array of"):
 		element_type = t.replace("array of ", "").split(" ")[0]
-		loop = "var i_$ = 0\n\t\t" \
-			   "while (i_$ < $.length) {\n\t\t" \
-			   "\t%s\n\t\t" \
-			   "\ti_$ += 1\n\t\t" \
+		loop = "var i_$ = 0\n    " \
+			   "while (i_$ < $.length) {\n    " \
+			   "  %s\n    " \
+			   "  i_$ += 1\n    " \
 			   "}" % parametrize(niol_write(element_type), "$", "$(i_$)")
 		return loop
 
@@ -144,11 +144,11 @@ def niol_read(field_type: str, prefix: str = "val ") -> str:
 		return simple_dict.get("string")
 	if t.startswith("array of "):
 		element_type = t.replace("array of ", "").split(" ")[0]
-		loop = "var i_$ = 0\n\t\t" \
-			   "%s$ = new %s($Length)\n\t\t" \
-			   "while (i_$ < $Length) {\n\t\t" \
-			   "\t%s\n\t\t" \
-			   "\ti_$ += 1\n\t\t" \
+		loop = "var i_$ = 0\n    " \
+			   "%s$ = new %s($Length)\n    " \
+			   "while (i_$ < $Length) {\n    " \
+			   "  %s\n    " \
+			   "  i_$ += 1\n    " \
 			   "}" % (prefix, scala_type(field_type), parametrize(niol_read(element_type, ""), "$", "$(i_$)"))
 		return loop
 
@@ -222,14 +222,14 @@ def generate_packet_class(p: Packet, package: str) -> (str, str):
 			# Reads and writes the length before the actual value
 			# The length field isn't in the constructor nor in the builder.
 			length_fwrite = parametrize(niol_write(length_rawtype), "$", fname + ".length")
-			write.append("\t\t" + length_fwrite)
+			write.append("    " + length_fwrite)
 
 			length_fread = parametrize(niol_read(length_rawtype), "$", length_fname)
-			read.append("\t\t" + length_fread)
+			read.append("    " + length_fread)
 
 		name_and_type = "%s: %s" % (fname, ftype)
 		decl.append("var " + name_and_type)
-		b_decl.append("\tprivate[this] var %s = _" % name_and_type)
+		b_decl.append("  private[this] var %s = _" % name_and_type)
 
 		if ftype == "String":
 			imports.add("import java.nio.charset.StandardCharsets.UTF_8")
@@ -244,10 +244,10 @@ def generate_packet_class(p: Packet, package: str) -> (str, str):
 			imports.add("import com.electronwill.utils.{Pi2, InvPi2}")
 
 		fwrite = parametrize(niol_write(rawtype), "$", fname)
-		write.append("\t\t" + fwrite)
+		write.append("    " + fwrite)
 
 		fread = parametrize(niol_read(rawtype), "$", fname)
-		read.append("\t\t" + fread)
+		read.append("    " + fread)
 
 		global todo_count
 		global unhandled_type_count
@@ -261,36 +261,36 @@ def generate_packet_class(p: Packet, package: str) -> (str, str):
 		construct.append(fname)
 
 		fcapital = to_pascal_case(field.name_snake)
-		with1 = "\tdef with%s(%s): %s[P with %s] = {" % (fcapital, name_and_type, bname, fcapital)
-		with2 = "\t\tthis.%s = %s" % (fname, fname)
-		with3 = "\t\tthis.asInstanceOf[%s[P with %s]]" % (bname, fcapital)
-		with4 = "\t}"
+		with1 = "  def with%s(%s): %s[P with %s] = {" % (fcapital, name_and_type, bname, fcapital)
+		with2 = "    this.%s = %s" % (fname, fname)
+		with3 = "    this.asInstanceOf[%s[P with %s]]" % (bname, fcapital)
+		with4 = "  }"
 		b_with.append("\n".join([with1, with2, with3, with4]))
 
 		if rawtype == "position":  # build with Vec3i instead of long
-			with1 = "\tdef with%s(v: Vec3i): %s[P with %s] = {" % (fcapital, bname, fcapital)
-			with2 = "\t\tthis.%s = ((v.x & 0x3ffffff) << 38) | ((v.y & 0xfff) << 26) | (v.z & 0x3ffffff)" % fname
-			with3 = "\t\tthis.asInstanceOf[%s[P with %s]]" % (bname, fcapital)
-			with4 = "\t}"
+			with1 = "  def with%s(v: Vec3i): %s[P with %s] = {" % (fcapital, bname, fcapital)
+			with2 = "    this.%s = ((v.x & 0x3ffffff) << 38) | ((v.y & 0xfff) << 26) | (v.z & 0x3ffffff)" % fname
+			with3 = "    this.asInstanceOf[%s[P with %s]]" % (bname, fcapital)
+			with4 = "  }"
 			b_with.append("\n".join([with1, with2, with3, with4]))
 
-			get1 = "\tdef vec%s: Vec3i = {" % fcapital
-			get2 = "\t\tval x = %s >> 38" % fname
-			get3 = "\t\tval y = (%s >> 26) & 0xfff" % fname
-			get4 = "\t\tval z = %s << 38 >> 38" % fname
-			get5 = "\t\tnew Vec3i(x, y, z)"
-			get6 = "\t}"
+			get1 = "  def vec%s: Vec3i = {" % fcapital
+			get2 = "    val x = %s >> 38" % fname
+			get3 = "    val y = (%s >> 26) & 0xfff" % fname
+			get4 = "    val z = %s << 38 >> 38" % fname
+			get5 = "    new Vec3i(x, y, z)"
+			get6 = "  }"
 			additional_getters.append("\n".join([get1, get2, get3, get4, get5, get6]))
 		elif rawtype == "angle":
-			with1 = "\tdef with%s(angle: Float): %s[P with %s] = {" % (fcapital, bname, fcapital)
-			with2 = "\t\tthis.%s = (angle * InvPi2 * 256f).toByte  " % fname
-			with3 = "\t\tthis.asInstanceOf[%s[P with %s]]" % (bname, fcapital)
-			with4 = "\t}"
+			with1 = "  def with%s(angle: Float): %s[P with %s] = {" % (fcapital, bname, fcapital)
+			with2 = "    this.%s = (angle * InvPi2 * 256f).toByte  " % fname
+			with3 = "    this.asInstanceOf[%s[P with %s]]" % (bname, fcapital)
+			with4 = "  }"
 			b_with.append("\n".join([with1, with2, with3, with4]))
 
-			get1 = "\tdef rad%s: Float = {" % fcapital
-			get2 = "\t\t%s * Pi2 / 256f" % fname
-			get3 = "\t}"
+			get1 = "  def rad%s: Float = {" % fcapital
+			get2 = "    %s * Pi2 / 256f" % fname
+			get3 = "  }"
 			additional_getters.append("\n".join([get1, get2, get3]))
 
 		traits.append("sealed trait %s" % fcapital)
@@ -302,7 +302,7 @@ def generate_packet_class(p: Packet, package: str) -> (str, str):
 	packet_constructing = ", ".join(construct)
 	builder_fields = "\n".join(b_decl)
 	builder_setters = "\n\n".join(b_with)
-	traits_declaration = "\n\t".join(traits)
+	traits_declaration = "\n  ".join(traits)
 	mixin_declaration = " with ".join(complete)
 
 	# TODO import tuubes.core.network.packets or something like this
@@ -316,21 +316,21 @@ package %s
 
 /** Packet class auto-generated by DataTractor */
 final class %s(%s) extends Packet {
-	override def write(out: NiolOutput): Unit {
+  override def write(out: NiolOutput): Unit {
 %s
-	}
+  }
 	
-	override def id = %s.id
+  override def id = %s.id
 
 %s	
 }
 object %s extends PacketObj[%s] {
-	override val id = %d
+  override val id = %d
 	
-	override def read(in: NiolOutput): %s {
+  override def read(in: NiolOutput): %s {
 %s
-		new %s(%s)
-	}
+    new %s(%s)
+  }
 }
 /** Packet builder auto-generated by DataTractor */
 final class %s[P <: %s] extends PacketBuilder[%s, Complete] {
@@ -338,13 +338,13 @@ final class %s[P <: %s] extends PacketBuilder[%s, Complete] {
 
 %s
 
-	override def build()(implicit evidence: P =:= Complete) {
-		new %s(%s)
-	}
+  override def build()(implicit evidence: P =:= Complete) {
+    new %s(%s)
+  }
 }
 object %s {
-	%s
-	type Complete = %s
+  %s
+  type Complete = %s
 }
 """ % (package,
 	   "\n".join(imports),
@@ -391,9 +391,9 @@ import org.tuubes.craft.MinecraftProtocol
 
 /** Protocol class auto-generated by DataTractor */
 object %s extends MinecraftProtocol {
-	%s
+  %s
 }
-""" % (package, sname, "\n\t".join(registrations))
+""" % (package, sname, "\n  ".join(registrations))
 	return sname, sclass.strip()
 
 
